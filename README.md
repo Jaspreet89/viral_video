7e1f7d0645370f76b20e623399a9042ce6988589
Jaspreet Singh <jaashy.singh@gmail.com>
2016-10-19 12:28:13 +0800

    change

---
+++ README.md
@@ -0,0 +1,19 @@
+socket.io-video
+===============
+
+FaceTime Camera share using socket.io 
+
+Install
+----------
+
+```shell
+$ git clone https://github.com/yosuke-furukawa/socket.io-video.git
+$ cd socket.io-video
+$ npm install
+$ node app.js
+```
+
+Lauch your webcam and use chrome browser.
+
+
+<img src="https://i.cloudup.com/Nj2E-YD-z4-3000x3000.jpeg" width=40% height=40% />
---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var http = require('http');
+var server = http.Server(app);
+var io = require('socket.io')(server);
+var users_key = 'users';
+var sioredis = require('socket.io-redis');
+/*io.adapter(sioredis({ 
+  host: 'localhost', 
+  port: 6379,
+}));*/
+app.use(express.static(__dirname + "/public"));
+io.on('connection', function(socket){
+  socket.on('image', function(data){
+    socket.broadcast.volatile.emit('image', {
+      id: socket.id,
+      blob: data
+    });
+  });
+  socket.on("disconnect", function(){
+    socket.broadcast.emit("leave", {
+      id: socket.id
+    });
+  });
+});
+server.listen(process.env.PORT || 3000);
